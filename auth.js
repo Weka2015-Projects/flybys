780cc03a6900d927e9ebb29b0778aca9703e53f5
colin boyd <email": null>
2016-02-04 18:10:06 +1300

    got sessions running via the past koa-sessions exercise, need to cater to our db

---
+++ auth.js
@@ -0,0 +1,23 @@
+var passport = require('koa-passport')
+
+require('dotenv').config()
+
+var user = { id: 1, username: 'test' }
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+  done(null, user)
+})
+
+var LocalStrategy = require('passport-local').Strategy
+passport.use(new LocalStrategy(function(username, password, done) {
+  // retrieve user ...
+  if (username === 'test' && password === 'test') {
+    done(null, user)
+  } else {
+    done(null, false)
+  }
+}))
